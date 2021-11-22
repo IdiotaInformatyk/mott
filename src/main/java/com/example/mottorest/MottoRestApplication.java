ef9b0fda38fc72f5d81a3144b73db757fdb99522
IdiotaInformatyk <92095721+IdiotaInformatyk@users.noreply.github.com>
2021-11-22 21:12:53 +0100

    Add files via upload

---
+++ src/main/java/com/example/mottorest/MottoRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.mottorest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MottoRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MottoRestApplication.class, args);
+    }
+
+}
